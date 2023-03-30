1e9b1b0a4234d9bc3ed56305ee8b1c8d18231f76
Tomás Pacheco <tomas.pacheco.1233435@sga.pucminas.br>
2023-03-30 10:54:52 -0300

    Mover funcionalidade de dano para a própria classe

--- TI RPG/Assets/Player/PlayerMovement.cs
+++ TI RPG/Assets/Player/PlayerMovement.cs
@@ -12,47 +12,7 @@ namespace Player
    
         private Vector3 targetPosition;
         private Camera mainCamera;
-             
-        [SerializeField] private int vidas =2;
-        public int Vidas
-        {
-            get { return vidas; }
-            set
-            {
-                vidas = value;
-                if (vidas <= 0)
-                {
-                    Time.timeScale = 0;
-                    Debug.Log("Game Over");
-                    GameOverController.Instance.GameOver();
-                }
-            }
-        }
-        void OnCollisionEnter(Collision collision)
-        {
-            if (collision.collider.gameObject.CompareTag("Inimigo"))
-            {
-                Debug.Log("oi");
-                Vidas -= 1;
-                StartCoroutine(TomarDano());
-            }
-        }
-        IEnumerator TomarDano()
-        {
-            MeshRenderer mr = gameObject.GetComponent<MeshRenderer>();
-            Color selfColor = mr.material.color;
-            Color damageColor = Color.red;
-            velocidade *= 2;
-            for (int i = 0; i < 3; i++)
-            {
-                yield return new WaitForSeconds(0.3f);
-                mr.material.color = damageColor;
-                yield return new WaitForSeconds(0.3f);
-                mr.material.color = selfColor;   
-            }
-            velocidade /= 2;
-            
-        }
+        
         private void Awake()
         {
             mainCamera = Camera.main;
