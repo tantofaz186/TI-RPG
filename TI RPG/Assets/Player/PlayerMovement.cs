e75d590ae10b343b33186b9fd82233dc07b4c7bf
Joaoherval <89783549+Kennykiller36@users.noreply.github.com>
2023-03-29 22:14:57 -0300

    Foi feito o dano e morte do player.

--- TI RPG/Assets/Player/PlayerMovement.cs
+++ TI RPG/Assets/Player/PlayerMovement.cs
@@ -7,8 +7,34 @@ namespace Player
 {
     public class PlayerMovement : Agente
     {
+   
         private Vector3 targetPosition;
         private Camera mainCamera;
+             [SerializeField]
+        private int vidas =2;
+        private GameObject Gameoverscreen;
+        public int Vidas
+        {
+            get { return vidas; }
+            set
+            {
+                vidas = value;
+                if (vidas <= 0)
+                {
+                    vidas = 0;
+                    Time.timeScale = 0;
+                    Debug.Log("Game Over");
+                }
+            }
+        }
+        void OnCollisionEnter(Collision collision)
+        {
+            if (collision.collider.gameObject.tag == "Inimigo")
+            {
+                Debug.Log("oi");
+                Vidas -= 1;
+            }
+        }
         private void Awake()
         {
             mainCamera = Camera.main;
