e639425eb39869ea204bfc938fe57c8156fd6acb
Tomás Pacheco <tomaspedron@gmail.com>
2023-03-31 10:27:43 -0300

    Esconder camera basePosition

--- TI RPG/Assets/CameraFollowTarget.cs
+++ TI RPG/Assets/CameraFollowTarget.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class CameraFollowTarget : MonoBehaviour
 {
     [SerializeField] Transform player;
-    [SerializeField]Vector3 basePosition;
+    Vector3 basePosition;
     // Update is called once per frame
     private void Awake()
     {
