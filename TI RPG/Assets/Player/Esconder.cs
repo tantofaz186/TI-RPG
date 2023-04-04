604894c3bced659b248b537de3c68b39e3c861ba
Tomás Pacheco <tomas.pacheco.1233435@sga.pucminas.br>
2023-04-04 12:09:25 -0300

    Create Esconder Script

---
+++ TI RPG/Assets/Player/Esconder.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+namespace Player
+{
+    public class Esconder : MonoBehaviour
+    {
+        //Player esconde dentro de objetos
+        //Quando o player estiver escondido, ele não pode ser visto pelo inimigo
+        //Ao clicar no botão de esconder, o player deve esconder-se no objeto mais próximo
+        //Ao clicar no botão de esconder novamente, o player deve sair do objeto onde está escondido
+
+        private void Awake()
+        {
+            
+        }
+
+        private void Update()
+        {
+            
+        }
+    }
+}
