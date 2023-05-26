03d3c4915ca8878bf765623e704f546cb6efab48
Tomás Pedron Turchi Pacheco <tomaspedron@gmail.com>
2023-05-25 22:07:53 -0300

    .

--- TI RPG/Assets/CameraFollowTarget.cs
+++ TI RPG/Assets/CameraFollowTarget.cs
@@ -6,22 +6,24 @@ using UnityEngine;
 public class CameraFollowTarget : MonoBehaviour
 {
     [SerializeField] Transform player;
-    Vector3 basePosition;
-    public float sensitivity = 5.0f; 
-    private float currentAngle = 64.493f;
+    Vector3 lastPlayerPosition;
+    public float sensitivity = 5.0f;
+    private float currentAngle;
+
     private void Awake()
     {
-        basePosition = transform.position - player.transform.position;
+        lastPlayerPosition = player.position;
     }
 
     void Update () {
-        transform.position = player.transform.position + basePosition;
-        if (Input.GetMouseButtonDown(2))
+        if (Input.GetMouseButton(2))
         {
-            currentAngle += Input.GetAxis("Mouse X") * sensitivity;
-            Quaternion rotation = Quaternion.Euler(64.493f, currentAngle, 0);
-            transform.rotation = rotation;
+            currentAngle = Input.GetAxis("Mouse X") * sensitivity;
+            transform.RotateAround (player.position, Vector3.up , currentAngle);
+            
         }
+        transform.Translate(player.position - lastPlayerPosition, Space.World);
+        lastPlayerPosition = player.position;
     }
 
     
