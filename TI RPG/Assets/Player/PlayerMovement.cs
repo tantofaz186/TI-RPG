5f4985a47b36a466f7016ee2ee54750b8cf8b3e1
Tomás Pacheco <tomas.pacheco.1233435@sga.pucminas.br>
2023-06-06 12:11:22 -0300

    .

--- TI RPG/Assets/Player/PlayerMovement.cs
+++ TI RPG/Assets/Player/PlayerMovement.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
 using IA;
 using Skills;
 using UnityEngine;
@@ -15,6 +18,7 @@ namespace Player
             mainCamera = Camera.main;
             targetPosition = transform.position;
             corpo_fsm = gameObject.GetComponent<Animator>();
+            corpo_fsm.SetFloat("Mover", 0.5f);
         }
 
         protected override void Update()
@@ -23,50 +27,54 @@ namespace Player
             agente.speed = velocidade;
             if (Input.GetMouseButtonDown(1))
             {
-                targetPosition = transform.position;
                 if (Physics.Raycast(mainCamera.ScreenPointToRay(Input.mousePosition), out var hit))
                 {
-                    targetPosition = new Vector3(hit.point.x, transform.position.y, hit.point.z);
+                    targetPosition = new Vector3(hit.point.x, hit.point.y, hit.point.z);
                     Mover(targetPosition);
                 }
-                if (transform.position != targetPosition)
-                {
-                    corpo_fsm.SetBool("movimentando", true);
-
-                }
             }
-            Mover(targetPosition);
+            //Mover(targetPosition);
 
             #region Movimentacao/Mecanim
-
-            if (transform.position.x == targetPosition.x && transform.position.z == targetPosition.z)//Idle
+            if (agente.destination == transform.position)
             {
                 corpo_fsm.SetBool("movimentando", false);
                 Debug.Log("Parou");
             }
-            if (transform.position.x == targetPosition.x && transform.position.z == targetPosition.z && Input.GetKey(KeyCode.LeftControl) == true)//Agachar
+            else if(!Input.GetKey(KeyCode.LeftControl) && !Input.GetKey(KeyCode.LeftShift))
+            {
+                corpo_fsm.SetBool("movimentando", true);
+                velocidade = 3.0f;
+                StartCoroutine(LerpValue("Mover", 0.5f));
+            }
+            if (Input.GetKey(KeyCode.LeftControl))//Agachar
             {
                 corpo_fsm.SetBool("agachado", true);
+                velocidade = 1.75f;
+                StartCoroutine(LerpValue("Mover", 0f));
             }
             else corpo_fsm.SetBool("agachado", false);// Desagachar
 
             if (Input.GetKey(KeyCode.LeftShift))//Correr
             {
                 velocidade = 4.25f;
-                corpo_fsm.SetFloat("Mover", 0.5f);
-            }
-            else if (Input.GetKey(KeyCode.LeftControl))//Sneak
-            {
-                velocidade = 1.75f;
-                corpo_fsm.SetFloat("Mover", 1.0f);
-            }
-            else//Parar de correr
-            {
-                velocidade = 3.0f;
-                corpo_fsm.SetFloat("Mover", 0f);
+                StartCoroutine(LerpValue("Mover", 1f));
             }
 
+
             #endregion
+        }
+
+        private IEnumerator LerpValue(string variableName, float targetValue)
+        {
+            float currentValue = corpo_fsm.GetFloat(variableName);
+            while (currentValue - targetValue > 0.01f || currentValue - targetValue < -0.01f)
+            {
+                corpo_fsm.SetFloat(variableName, Mathf.Lerp(currentValue, targetValue, 0.1f));
+                currentValue = corpo_fsm.GetFloat(variableName);
+                yield return null;
+            }
+            corpo_fsm.SetFloat(variableName, targetValue);
         }
     }
 
