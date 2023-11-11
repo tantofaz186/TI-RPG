2da61964fd40353f62e4f0a2537dd6acc3dcb059
Joaoherval <89783549+Kennykiller36@users.noreply.github.com>
2023-11-11 15:39:59 -0300

    colocar primeira versao do codigo de ouvir e concertar bug de abrir o inventario

---
+++ TI RPG/Assets/Scripts/IA/OuvindoPlayer.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace IA
+{
+    public class OuvindoPlayer : Agente
+    {
+        public float areaDeAudicao = 5.0f;
+        public LayerMask detectionLayer;
+        [SerializeField] private float waitTimeWhenSuspicious = 1.5f;
+        [SerializeField] private InimigoUI inimigoUI;
+        [SerializeField] Transform alvo;
+        public Transform Alvo => alvo;
+
+        private void Awake()
+        {
+            alvo = GameObject.FindGameObjectWithTag("Player").transform;
+        }
+
+        private void Update()
+        {
+            if (!Input.GetKey(KeyCode.LeftControl) && !Input.GetKey(KeyCode.LeftShift))
+            {
+                areaDeAudicao = 5.0f;
+            }
+            else if (Input.GetKey(KeyCode.LeftControl))
+            {
+                areaDeAudicao = 3.0f;
+            }
+            else if (Input.GetKey(KeyCode.LeftShift))
+            {
+                areaDeAudicao = 7.0f;
+            }
+
+            Collider[] colliders = Physics.OverlapSphere(transform.position, areaDeAudicao);
+            if (colliders.Length > 0)
+            {
+                foreach (Collider collider in colliders)
+                {
+                    if (collider.gameObject.tag == "Player")
+                    {
+                        StartCoroutine(MoverAteOSom());
+                        //Debug.Log("Detected: " + collider.gameObject.name);
+                    }
+                    else
+                    {
+                        StopCoroutine(MoverAteOSom());
+                    }
+                }
+            }
+        }
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(transform.position, areaDeAudicao);
+        }
+        IEnumerator MoverAteOSom()
+        {
+            Vector3 lastKnownPosition = Alvo.position;
+            Mover(transform.position);
+            yield return new WaitForSeconds(waitTimeWhenSuspicious);
+        }
+    }
+}
