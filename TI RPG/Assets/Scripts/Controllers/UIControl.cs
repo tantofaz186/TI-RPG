d1bd2263d354fc8f7d8a625beb154ffc40f75d20
GabrielCodificante <gabriel.slacorte@outlook.com.br>
2023-12-14 16:45:55 -0300

    UI Prefab Refence Update

--- TI RPG/Assets/Scripts/Controllers/UIControl.cs
+++ TI RPG/Assets/Scripts/Controllers/UIControl.cs
@@ -14,6 +14,7 @@ public class UIControl : MonoBehaviourSingletonPersistent<UIControl> {
     public GameObject dialogBox;
     public GameObject pauseMenu;
     public GameObject quickInventory;
+    public GameObject menuInventory;
     public GameObject inventory;
 
     private void Start()
@@ -26,7 +27,7 @@ public class UIControl : MonoBehaviourSingletonPersistent<UIControl> {
     private void ResetUI(Scene arg0, LoadSceneMode arg1)
     {
         mainMenu.SetActive(arg0.buildIndex == 0); //Só ativar se estiver na cena do menu principal
-        inventory.SetActive(arg0.buildIndex != 0);
+        menuInventory.SetActive(arg0.buildIndex != 0);
         quickInventory.SetActive(arg0.buildIndex != 0);
         skillTreeMenu.SetActive(false);
         dialogBox.SetActive(false);
@@ -61,7 +62,7 @@ public class UIControl : MonoBehaviourSingletonPersistent<UIControl> {
 
     public void SetInventoryActive(bool active)
     {
-        inventory.SetActive(active);
+        menuInventory.SetActive(active);
     }
 
     private void Update()
@@ -109,16 +110,14 @@ public class UIControl : MonoBehaviourSingletonPersistent<UIControl> {
     {
         if (inventoryOpen != false)
         {
-            inventory.GetComponent<Animator>().SetTrigger("fechar");
-            quickInventory.GetComponent<Animator>().SetTrigger("fechar");
+            menuInventory.GetComponent<Animator>().SetTrigger("fechar");
             StartCoroutine(liberarMudanca());
             inventoryOpen = false;
             Debug.Log("Estado aberto: " + inventoryOpen);
         }
         else
         {
-            inventory.GetComponent<Animator>().SetTrigger("abrir");
-            quickInventory.GetComponent<Animator>().SetTrigger("abrir");
+            menuInventory.GetComponent<Animator>().SetTrigger("abrir");
             inventoryOpen = true;
             Debug.Log("Estado aberto: " + inventoryOpen);
         }
