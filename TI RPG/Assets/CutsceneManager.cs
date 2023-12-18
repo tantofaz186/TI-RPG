286d453232a473d58f3c4e21b7bc40c3bb687aed
Tomás Pedron Turchi Pacheco <tomaspedron@gmail.com>
2023-12-17 23:29:03 -0300

    skip cutscene and precompute outline

--- TI RPG/Assets/CutsceneManager.cs
+++ TI RPG/Assets/CutsceneManager.cs
@@ -12,6 +12,19 @@ public class CutsceneManager : MonoBehaviour
         player.loopPointReached += ChangeSceneWhenVideoEnds;
     }
 
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Return))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+
+    private void OnDisable()
+    {
+        player.loopPointReached -= ChangeSceneWhenVideoEnds;
+    }
+
     private void ChangeSceneWhenVideoEnds(VideoPlayer source)
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
