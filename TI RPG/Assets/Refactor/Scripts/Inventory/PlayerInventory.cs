4cad9204450e4c05baca63e24ce914294e2d730a
Tomás Pedron Turchi Pacheco <tomaspedron@gmail.com>
2023-12-18 09:45:04 -0300

    force add recipes and items to UI

--- TI RPG/Assets/Refactor/Scripts/Inventory/PlayerInventory.cs
+++ TI RPG/Assets/Refactor/Scripts/Inventory/PlayerInventory.cs
@@ -351,6 +351,22 @@ namespace Rpg.Crafting
         }
 
         #endregion
+
+        #if UNITY_EDITOR
+        private void OnValidate()
+        {
+            items = GetAllInstances<Item>().ToArray();
+            recipes = GetAllInstances<Recipe>().ToArray();
+        }
+
+        private IEnumerable<T> GetAllInstances<T>() where T : ScriptableObject
+        {
+            return AssetDatabase.FindAssets($"t: {typeof(T).Name}").ToList()
+                .Select(AssetDatabase.GUIDToAssetPath)
+                .Select(AssetDatabase.LoadAssetAtPath<T>)
+                .ToList();
+        }
+        #endif
     }
 
     #if UNITY_EDITOR
